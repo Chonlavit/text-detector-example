5126f7690af52c435633987bcea367a8588b7613
Chonlavit <chonlavit.puangtang@priorsolution.co.th>
2021-08-19 17:15:18 +0700

    first commit

---
+++ textDetector/textDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  textDetector
+//
+//  Created by X10-01-18 on 19/8/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
